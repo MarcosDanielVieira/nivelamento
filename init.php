b5a8a2525ca47b3bc9306b7e87111a6634ed7862
Marcos Daniel <marcosdaniel.developer@hotmail.com>
2022-10-29 10:15:05 -0300

    Tentativa de hospedagem do site

--- init.php
+++ init.php
@@ -6,7 +6,7 @@ date_default_timezone_set('America/Sao_Paulo');
 
 // conexão com o banco de dados
 
-define('BD_SERVIDOR', 'localhost');
+define('BD_SERVIDOR', '10.123.0.211');
 define('BD_USUARIO', 'root');
-define('BD_SENHA', '');
-define('BD_BANCO', 'nivelamento');
+define('BD_SENHA', 'Om5r8z6BC#');
+define('BD_BANCO', 'mardan38_nivelamento');
